064e3a9afe7a0de51d65daec8508110ed0af561e
Rustam Shamaev <r.shamaev@socialquantum.com>
2021-06-26 19:36:35 +0300

    Init

---
+++ Source/CubeMind/CubeMindGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CubeMindGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBEMIND_API ACubeMindGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
